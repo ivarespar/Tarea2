f0b1caa7e8631f473d5cd7d2f8a43444894300da
IvanEspinosa <espinosa.arellano@gmail.com>
2021-06-24 03:11:24 -0500

    Agregar archivos de proyecto.

---
+++ MVVM/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MVVM.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) => { await Navigation.PushAsync(new Operations()); };
+        }
+    }
+}
